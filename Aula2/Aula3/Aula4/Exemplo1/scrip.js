d90c1a386eb25d92c0b39a2c0625991d8fd28d97
Felipe-Shalon <felipeschunkoficial@hotmail.com>
2025-09-23 19:48:00 -0300

    exercicio htmls e js

---
+++ Aula2/Aula3/Aula4/Exemplo1/scrip.js
@@ -0,0 +1,16 @@
+/*Exemplo 1*/
+
+/* Felipe Schunk */
+
+//comentário de uma linha
+
+console.log("Olá mundo")
+
+console.log("Olá mundo parte 2")
+
+prompt("Qual é seu nome?")
+
+const nome = "Felipe Schunk"
+const idade = 26
+
+console.log("O nome é:", nome, "e sua idade é:", idade)
---
+++ Aula2/Aula3/Aula4/Exercício/scrip.js
@@ -0,0 +1,16 @@
+/*Exemplo 1*/
+
+/* Felipe Schunk */
+
+//comentário de uma linha
+
+console.log("Olá mundo")
+
+console.log("Olá mundo parte 2")
+
+const nome = "Felipe"
+const sobrenome = "Schunk"
+const idade = 26
+const EuSouEstudante = true
+
+console.log("Olá!", "Meu nome é ", nome, sobrenome, "e eu tenho", idade, "anos", "e sou estudante", EuSouEstudante)
