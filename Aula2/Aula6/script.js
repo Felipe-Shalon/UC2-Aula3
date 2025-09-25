c53cee87b5f5341164a2f15ce4d070d75606de0f
Felipe-Shalon <felipeschunkoficial@hotmail.com>
2025-09-24 21:52:36 -0300

    exercicios de interpretacao de codigos

---
+++ Aula2/Aula6/script.js
@@ -0,0 +1,43 @@
+
+//Interpretação de código
+/*Exercicio 1*/
+let a = 10 //let cria uma variável 'a' com valor de 10 que pode ser alterada ao decorrer do programa
+let b = 10 //let cria uma variável 'b' com valor de 10 que pode ser alterada ao decorrer do programa
+console.log(b)//console.log imprimi o valor da variável 'b'
+b = 5//váriavel 'b' de valor 5
+console.log(a, b)//Imprime os valores de 'a' e 'b'.
+
+/*Exercicio 2:*/
+let d = 10 //let cria uma variável 'd' de valor 10 que pode ser alterada ao decorrer do programa
+let e = 10//let cria uma variável 'e' de valor 10 que pode ser alterada ao decorrer do programa
+c = d//Variável 'c' de valor igual 'b'
+e = c//Variável 'e' de valor igual 'c'
+d = e//Variável 'd' de valor igual 'e'
+console.log(d, e, c)//Imprimi os valores das variáveis 'd', 'e' e 'c'.
+
+//Exercício 3
+let p = prompt("Quantas horas você trabalha por dia?")
+//let informa uma variável 'p' onde o 'p' indica quantas horas são trabalhadas por dia
+let t = prompt("Quanto você recebe por dia?")
+//let informa uma variável 't' onde o 't' indica quanto se recebe por dia trabalhados.
+console.log("Você recebe", t, "/", p, "por hora")
+//console.log imprimi quanto a pessoa recebe por dia e por hora.
+//pode-se substituir 'p' por 'horasTrabalhadas' e 't' por 'diasTrabalhados'.
+
+let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+let diasTrabalhados = prompt("Quanto você recebe por dia?")
+console.log("Você recebe", horasTrabalhadas, "por dia e trabalha", diasTrabalhados)
+
+/*1) Construa um programa, seguindo os seguintes passos:
+1. Declare uma variável para armazenar um nome, sem atribuir um valor.
+2. Declare uma variável para armazenar uma idade, sem atribuir um valor.
+3. Imprima na tela o tipo dessas variáveis que acabou de criar, usando o comando
+`typeof`.
+4. Reflita: por que esse tipo foi impresso? Escreva a resposta em um comentário de
+código.
+5. Pergunte ao usuário seu nome e sua idade, atribuindo esses dois valores às variáveis
+que acabou de criar.
+6. Imprima na tela o tipo dessas variáveis. Escreva em um comentário de código.
+7. Para finalizar, imprima na tela a mensagem: "Olá `nome`, você tem `idade` anos". Onde
+`nome` e `idade` são os valores que o usuário inseriu.*/
+
