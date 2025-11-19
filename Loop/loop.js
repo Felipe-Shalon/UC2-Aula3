6c577edb4a4ac198d8c432aff390b360a80e085f
Felipe-Shalon <felipeschunkoficial@hotmail.com>
2025-11-19 17:34:24 -0300

    Aulas

---
+++ Loop/loop.js
@@ -0,0 +1,22 @@
+var c = 1 //var = variavel
+while (c <= 6){
+    console.log("Tudo bem")
+    c++ //significa a mesma coisa que C recebe c+1 C = C + 1
+}
+
+var d = 1
+while (d <= 2){
+    console.log(`Passo ${d}`)
+    d++ 
+}
+
+
+
+/*
+console.log("Tudo bem")
+console.log("Tudo bem")
+console.log("Tudo bem")
+console.log("Tudo bem")
+console.log("Tudo bem")
+console.log("Tudo bem")
+*/
